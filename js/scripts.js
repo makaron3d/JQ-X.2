6929c283d394e0573d1ac0727c4e7170537faaa7
makaron3d <makaron3d@gmail.com>
2017-08-23 20:58:13 +0200

    032

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
-var span = $("span");
-    span.each(function(index, element) {
-});
-
+var span = $('span');
 span.each(function(index, element) {
-    if(index % 2 == 0) {
+    if(index % 2 === 0) {
 		$(element).css('color', 'red');
 	};
 });
@@ -12,25 +9,24 @@ span.each(function(index, element) {
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
-    var button = '<button class="btn" data-tmp="' + "Index to: " + index + '">Click me</button>'
-$(element).append(button)
-});
-    
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+    var button = '<button class="btn" data-tmp="Wartość: ' + index + '">Click me</button>';
+    $(element).append(button);
 });
 
-$(function(){
-  
-    $("body").append("<div><h1>Nic!</h1><p>Jakaś treść!</p></div>")
-  
-    var firstItem = $('div').find('h1')
- 
+    
+$('button').click(function() {
+    alert($(this).attr("data-tmp"));
+});
+
+$(function() { 
+    $('body').append('<div><h1>Nic!</h1><p>Jakaś treść!</p></div>');
+    var firstItem = $('div').find('h1');
     console.log(firstItem.text());
-  
-    firstItem.css("color", "blue")
+    firstItem.css("color", "blue");
 });
 
 $(function() {
     var p = $('div').find('p').last().css('color', 'green');
 });
+
+//Niektórzy mówią, ze w ostatniej linii funckji/obiektu, nie trzeba stawiac srednika. Praktyczniej jednak to robic? Jest jakas komenda w GIT, zeby dodawac wszystkie pliki lacznie, zamiast robic: git add <plik>? Probowalem Git add -p i git add files i nie dzialaja.
